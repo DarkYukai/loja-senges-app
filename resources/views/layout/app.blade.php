970e380ea17654aaa3e90d2c5a0fb1e4bf2b1593
DarkYukai <jeanmarcondes2024@gmail.com>
2024-09-24 14:49:39 +0000

    atualização

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,15 +12,18 @@
 <body>
     <!--cabecalho -->
     <header>
-
+        <!--carregar o portal -->
+        @include('partials.navbar')
     </header>
     <!--principal -->
     <main>
-        @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
     </main>
     <!--rodape -->
     <footer>
-
+        <!-- -->
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,6 +2,33 @@
 <!-- passar o nome da pagina para o titulo-->
 @section('title', 'Home')
 
+<!--inserir no yield do app sheet este fragmento-->
 @section('content')
-    <h1>Loja Senges</h1>
+<style>
+    .home-margin{
+        margin-top:10px;
+    }
+</style>
+<div class="row">
+    @foreach($produtos as $produto)
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div class="card h-100">
+            <img src="{{$produto->imagem}}" alt="{{$produto->nome}}" class="card-img-top">
+            <div class="car-body">
+                <h5 class="card-title">
+                    {{Str::limit($produto->nome,30)}}
+                </h5>
+                <p class="card-text">
+                    {{Str::limit($produto->nome,50)}}
+                </p>
+                <a href="/site/produtos/{{$produto->slug}}"
+                    class="btn btn-primary">
+                    <i class="bi bi-eye">Ver</i>
+                </a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
+
 @endsection
